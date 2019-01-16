712876a84733b5d1863c2a8b6be79299c91f786a
cfcodefans <cf.chenfan@gmail.com>
2019-01-16 17:59:27 +0800

    ..

--- cw-project-baseline-spring-boot/frontend/js/simple.js
+++ cw-project-baseline-spring-boot/frontend/js/simple.js
@@ -1,5 +1,11 @@
-function cleanText(ev) {
+function cleanInput(ev) {
     var t = $(ev.target).siblings()[0]
-    if (!(t && "INPUT" == t.tagName.toUpperCase() && t.type=="text" || t.type=="password")) return
+    if (!(t && "INPUT" == t.tagName.toUpperCase()
+            && (t.type=="text"
+                || t.type=="password"
+                || t.type=="mail"
+                || t.type=="tel"
+                || t.type=="number"
+                || t.type=="file"))) return
     t.value = ""
 }
